2a5c11a5a7ce695fa37da27f89397e127829af59
GUNJAN1999 <gunjanbbps62@gmail.com>
2020-10-21 14:32:30 +0530

    warnings handled

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,10 @@ import VideoDetail from './VideoDetail'
 class App extends React.Component
 {
 	state={videos:[] ,selectedVideo:null};
+	componentDidMount()
+	{
+		this.onTermSubmit('buildings');
+	}
 	onVideoSelect=(video)=>
 	{
 		this.setState({selectedVideo:video});
@@ -21,22 +25,27 @@ class App extends React.Component
 			}
 		});
 		console.log(response);
-		this.setState({videos:response.data.items});
+		this.setState({videos:response.data.items,
+			selectedVideo:response.data.items[0]});
 	};
 	render()
 	{
 		return(
 			<div className="ui container">
-				<SearchBar
-				onFormSubmit={this.onTermSubmit}/>
-				
-				FOUND : {this.state.videos.length}
-
-				<VideoDetail video={this.state.selectedVideo}/>
-				<VideoList
-				onVideoSelect={this.onVideoSelect}
-				videos={this.state.videos}
-				/>
+				<SearchBar onFormSubmit={this.onTermSubmit}/>
+				FOUND : {this.state.videos.length} Videos
+				<div className="ui grid">
+					<div className="ui row">
+						<div className="eleven wide column">
+							<VideoDetail video={this.state.selectedVideo}/>
+						</div>
+						<div className="five wide column">
+							<VideoList
+							onVideoSelect={this.onVideoSelect}
+							videos={this.state.videos}/>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
